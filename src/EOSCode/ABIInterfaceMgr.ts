966b62a78464bed88f2de88f7eb1818097fcce53
CityOfStars <cityofstars90@gmail.com>
2018-09-12 23:26:09 +0900

    add ABIInterfaceMgr

---
+++ src/EOSCode/ABIInterfaceMgr.ts
@@ -0,0 +1,167 @@
+'use strict';
+
+import * as fs from 'fs';
+
+export class ABIInterfaceMgr {
+
+    public getABIWebviewContent(abiPath : string, contractUser : string) {
+        let abiContents = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
+        //console.log(JSON.stringify(abiContents.actions, null, 4));
+
+        let functionNames = [];
+        let actions = abiContents.actions;
+        for (let i = 0; i < actions.length; i++) {
+            let obj = actions[i];
+            //console.log(obj['name']);   // private names
+            functionNames.push(obj['name']);
+        }
+
+        let tableNames = [];
+        let tables = abiContents.tables;
+        for (let i = 0; i < tables.length; i++) {
+            let table = tables[i];
+            tableNames.push(table.name);
+        }
+
+        // MUST REFACTOR IT
+        let contents =
+            `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <script>
+        const vscode = acquireVsCodeApi();
+        `
+        for (let i = 0; i < functionNames.length; i++) {
+            let functionName = functionNames[i];
+            contents += `function on${functionName}()
+        {
+            let accountForm = document.forms["user-account"];
+            let account = accountForm.getElementsByTagName("INPUT")[0].value;
+
+            let t = account + '$${functionName}()';
+            let form = document.forms["func${functionName}"];
+            let inputElements = form.getElementsByTagName("INPUT");
+
+            let isFirst = true;
+            for (let i =0; i< inputElements.length; i++)
+            {
+                if(inputElements[i].getAttribute("type") != "submit")
+                {
+                    if(!isFirst)
+                    {
+                        t += ',';
+                    }
+                    isFirst = false;
+
+                    //let paramName = inputElements[i].getAttribute("name");
+                    let value = inputElements[i].value;
+
+                    t += '\"' + value + '\"';
+                }
+            }
+            
+            vscode.postMessage({
+                command: 'pushAction',
+                text: t
+            });
+
+            return false;
+        }
+        `
+        }
+
+        for (let i = 0; i < tableNames.length; i++) {
+            let tableName = tableNames[i];
+            contents +=
+                `
+        function get${tableName}()
+        {
+            let form = document.forms["table${tableName}"];
+            let scope = form.getElementsByTagName("INPUT")[0].value;
+            //let inputElements = form.getElementsByTagName("INPUT"); // scope
+
+            //let scope = inputElements[0].value;
+        
+            //let t = scope + '$' + tableName;
+            let t = '${tableName}@' + scope;
+
+            vscode.postMessage({
+                command: 'getTable',
+                text: t
+            });
+
+            return false;
+        }
+        `;
+        }
+
+        // user account
+        contents += `</script><br/>
+    <h1>Setting</h1><hr/>
+    <form name="user-account">
+        user account : <input type="text" name="account" value="${contractUser}" />
+    </form><br/><br/>
+    <h1>Functions</h1><hr/>
+    `
+        let structs = abiContents.structs;
+        for (let i = 0; i < structs.length; i++) {
+            let obj = structs[i];
+            //console.log(obj['name']);   // structs names
+            let functionName = obj['name'];
+            //console.log("rammerchoi : " + functionNames.indexOf('rammerchoi'))
+            if (functionNames.indexOf(functionName) == -1)
+                continue;
+
+            // obj == function
+            console.log(functionName);
+            let fields = obj['fields'];
+            let nameTag = `<br/><br/><h2>${functionName}</h2><hr/>`;
+            contents += nameTag;
+
+            contents += `<form onsubmit="return on${functionName}();" name="func${functionName}" id="${functionName}">
+        `;
+
+            for (let k = 0; k < fields.length; k++) {
+                let f = fields[k];
+                let fieldName = f['name'];
+                let fieldType = f['type'];
+                //            let nodeName = `${functionName}_${fieldName}`;
+                let tag = `${fieldName}(${fieldType}) : <input type="text" name="${fieldName}" id="${fieldName}"/><br/><br/>
+            `;
+
+                contents += tag;
+            }
+
+            contents += `<input type="submit" value="Push Action" /></form>
+        
+        `;
+        }
+
+        //TODO - form : input table scope & get table
+        contents += `<br/><br/><h1>Tables</h1><hr/>`;
+
+        let tableContents = '';
+        for (let i = 0; i < tableNames.length; i++) {
+            let tableName = tableNames[i];
+            tableContents += `<br/><br/><h2>${tableName}</h2><hr/>`;
+            tableContents +=
+                `       
+        <form name="table${tableName}" onsubmit="return get${tableName}();">
+            scope : <input type="text" name="scope" />
+            <input type="submit" value="Get Table" />
+        </form><br/><br/>
+`
+        }
+
+        contents += tableContents;
+
+        contents += `</body></html>`;
+
+        return contents;
+    }
+}
--- src/EOSCode/EOSCode.ts
+++ src/EOSCode/EOSCode.ts
@@ -4,6 +4,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import { ConfigMgr } from './ConfigMgr';
+import { ABIInterfaceMgr } from './ABIInterfaceMgr';
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,6 +19,7 @@ export class EOSCode {
 
     // components
     public configMgr = new ConfigMgr();
+    public abiInterfaceMgr = new ABIInterfaceMgr();
 
     constructor(context: vscode.ExtensionContext, terminal: vscode.Terminal) {
         this.context = context;
@@ -240,7 +242,7 @@ export class EOSCode {
             });
 
         // And set its HTML content
-        panel.webview.html = this.getABIWebviewContent();
+        panel.webview.html = this.abiInterfaceMgr.getABIWebviewContent(this.getABIPath(), this.configMgr.configs.contract.user);
 
         panel.webview.onDidReceiveMessage(message => {
             switch (message.command) {
@@ -327,168 +329,7 @@ export class EOSCode {
         return path.join(dir, this.getOnlyFileName(this.configMgr.configs.buildTarget.wastSource) + ".wast");
     }
 
-    private getABIWebviewContent() {
-        let abiContents = JSON.parse(fs.readFileSync(this.getABIPath(), 'utf8'));
-        //console.log(JSON.stringify(abiContents.actions, null, 4));
-
-        let functionNames = [];
-        let actions = abiContents.actions;
-        for (let i = 0; i < actions.length; i++) {
-            let obj = actions[i];
-            //console.log(obj['name']);   // private names
-            functionNames.push(obj['name']);
-        }
-
-        let tableNames = [];
-        let tables = abiContents.tables;
-        for (let i = 0; i < tables.length; i++) {
-            let table = tables[i];
-            tableNames.push(table.name);
-        }
-
-        // MUST REFACTOR IT
-        let contents =
-            `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Coding</title>
-</head>
-<body>
-    <script>
-        const vscode = acquireVsCodeApi();
-        `
-        for (let i = 0; i < functionNames.length; i++) {
-            let functionName = functionNames[i];
-            contents += `function on${functionName}()
-        {
-            let accountForm = document.forms["user-account"];
-            let account = accountForm.getElementsByTagName("INPUT")[0].value;
-
-            let t = account + '$${functionName}()';
-            let form = document.forms["func${functionName}"];
-            let inputElements = form.getElementsByTagName("INPUT");
-
-            let isFirst = true;
-            for (let i =0; i< inputElements.length; i++)
-            {
-                if(inputElements[i].getAttribute("type") != "submit")
-                {
-                    if(!isFirst)
-                    {
-                        t += ',';
-                    }
-                    isFirst = false;
-
-                    //let paramName = inputElements[i].getAttribute("name");
-                    let value = inputElements[i].value;
-
-                    t += '\"' + value + '\"';
-                }
-            }
-            
-            vscode.postMessage({
-                command: 'pushAction',
-                text: t
-            });
-
-            return false;
-        }
-        `
-        }
-
-        for (let i = 0; i < tableNames.length; i++) {
-            let tableName = tableNames[i];
-            contents +=
-                `
-        function get${tableName}()
-        {
-            let form = document.forms["table${tableName}"];
-            let scope = form.getElementsByTagName("INPUT")[0].value;
-            //let inputElements = form.getElementsByTagName("INPUT"); // scope
-
-            //let scope = inputElements[0].value;
-        
-            //let t = scope + '$' + tableName;
-            let t = '${tableName}@' + scope;
-
-            vscode.postMessage({
-                command: 'getTable',
-                text: t
-            });
-
-            return false;
-        }
-        `;
-        }
-
-        // user account
-        contents += `</script><br/>
-    <h1>Setting</h1><hr/>
-    <form name="user-account">
-        user account : <input type="text" name="account" value="${this.configMgr.configs.contract.user}" />
-    </form><br/><br/>
-    <h1>Functions</h1><hr/>
-    `
-        let structs = abiContents.structs;
-        for (let i = 0; i < structs.length; i++) {
-            let obj = structs[i];
-            //console.log(obj['name']);   // structs names
-            let functionName = obj['name'];
-            //console.log("rammerchoi : " + functionNames.indexOf('rammerchoi'))
-            if (functionNames.indexOf(functionName) == -1)
-                continue;
-
-            // obj == function
-            console.log(functionName);
-            let fields = obj['fields'];
-            let nameTag = `<br/><br/><h2>${functionName}</h2><hr/>`;
-            contents += nameTag;
-
-            contents += `<form onsubmit="return on${functionName}();" name="func${functionName}" id="${functionName}">
-        `;
-
-            for (let k = 0; k < fields.length; k++) {
-                let f = fields[k];
-                let fieldName = f['name'];
-                let fieldType = f['type'];
-                //            let nodeName = `${functionName}_${fieldName}`;
-                let tag = `${fieldName}(${fieldType}) : <input type="text" name="${fieldName}" id="${fieldName}"/><br/><br/>
-            `;
-
-                contents += tag;
-            }
-
-            contents += `<input type="submit" value="Push Action" /></form>
-        
-        `;
-        }
-
-        //TODO - form : input table scope & get table
-        contents += `<br/><br/><h1>Tables</h1><hr/>`;
-
-        let tableContents = '';
-        for (let i = 0; i < tableNames.length; i++) {
-            let tableName = tableNames[i];
-            tableContents += `<br/><br/><h2>${tableName}</h2><hr/>`;
-            tableContents +=
-                `       
-        <form name="table${tableName}" onsubmit="return get${tableName}();">
-            scope : <input type="text" name="scope" />
-            <input type="submit" value="Get Table" />
-        </form><br/><br/>
-`
-        }
-
-        contents += tableContents;
-
-        contents += `</body></html>`;
-
-        return contents;
-    }
-
-    private showContractInterface() {
+    public showContractInterface() {
         // Create and show panel
         const panel = vscode.window.createWebviewPanel('contractInterface', "EOS Code - Contract Interface", vscode.ViewColumn.One,
             {
@@ -496,7 +337,7 @@ export class EOSCode {
             });
 
         // And set its HTML content
-        panel.webview.html = this.getABIWebviewContent();
+        panel.webview.html = this.abiInterfaceMgr.getABIWebviewContent(this.getABIPath(), this.configMgr.configs.contract.user);
 
         panel.webview.onDidReceiveMessage(message => {
             switch (message.command) {
@@ -538,4 +379,5 @@ export class EOSCode {
             }
         }, this, this.context.subscriptions);
     }
+
 }
